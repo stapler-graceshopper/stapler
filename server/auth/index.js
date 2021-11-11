b4a450b930d01a9ea485e16d074db2d4b48f9996
Sabi Kumar <skumar213@gmail.com>
2021-11-11 11:05:55 -0500

    added injection attack security

--- server/auth/index.js
+++ server/auth/index.js
@@ -14,7 +14,9 @@ router.post('/login', async (req, res, next) => {
 
 router.post('/signup', async (req, res, next) => {
   try {
-    const user = await User.create(req.body)
+    const {username, password, email, image, address} = req.body;
+
+    const user = await User.create({username, password, email, image, address})
     res.send({token: await user.generateToken()})
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
