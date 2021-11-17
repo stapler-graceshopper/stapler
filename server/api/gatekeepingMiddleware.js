f4a0e9b962da8db9e72158efcb84daadd904b08b
Sabi Kumar <skumar213@gmail.com>
2021-11-17 15:12:02 -0500

    check if user exists in gatekeeping middleware on server

--- server/api/gatekeepingMiddleware.js
+++ server/api/gatekeepingMiddleware.js
@@ -6,23 +6,34 @@ const {
 const requireToken = async (req, res, next) => {
   try {
     const user = await User.findByToken(req.headers.authorization);
-    req.user = user;
-    next();
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      const error = new Error("User needs to be logged in");
+      next(error);
+    }
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
 //makes sure user is an admin
-const isAdmin = (req, res,next) => {
-  if (req.user.type !== 'admin') {
-    return res.sendStatus(403)
-  } else {
-    next()
+const isAdmin = (req, res, next) => {
+  if (!req.user) {
+    const error = new Error("User needs to be logged in as Admin");
+    next(error);
+    return;
   }
-}
+
+  if (req.user.type !== "admin") {
+    return res.sendStatus(403);
+  } else {
+    next();
+  }
+};
 
 module.exports = {
   requireToken,
-  isAdmin
-}
+  isAdmin,
+};
